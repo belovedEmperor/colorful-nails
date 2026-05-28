6c2603145ba51b6bb7f638bbacfbcd77daa1e069
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-05-28 18:49:43 -0400

    feat: add Inter font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.rs", "./index.html"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
   plugins: [],
 };
