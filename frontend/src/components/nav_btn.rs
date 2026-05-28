c4428bae640c148215035414c435d987297f37db
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-05-28 18:33:29 -0400

    feat: add class prop to NavButton

--- frontend/src/components/nav_btn.rs
+++ frontend/src/components/nav_btn.rs
@@ -4,9 +4,10 @@ use leptos::prelude::*;
 pub fn NavButton<'a>(
     #[prop(default = "/")] href: &'a str,
     #[prop(default = "Navigate")] text_content: &'a str,
+    #[prop(optional, default = "")] class: &'a str,
 ) -> impl IntoView {
     view! {
-        <button class="nav-button">
+        <button class=format!("{class}")>
             <a href=href>{text_content}</a>
         </button>
     }
