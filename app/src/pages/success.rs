b6f27076591bf605425accce974510c7f31139a0
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-06-06 20:45:30 -0400

    feat: add success page

---
+++ app/src/pages/success.rs
@@ -0,0 +1,18 @@
+use crate::components::{errors::ErrorView, nav_btn::NavButton};
+use leptos::prelude::*;
+
+/// Success Page
+#[component]
+pub fn Success() -> impl IntoView {
+    view! {
+        <ErrorBoundary fallback=|errors| {
+            view! { <ErrorView errors=errors /> }
+        }>
+            <div class="section-padding section-container page-container justify-center section-container flex flex-col bg-background">
+                <h2 class="section-header">"Your Appointment Is Made!"</h2>
+                <p>"We will email you about the status of the appointment soon."</p>
+                <NavButton button_class="bg-primary button" href="/booking" text_content="Back" />
+            </div>
+        </ErrorBoundary>
+    }
+}
